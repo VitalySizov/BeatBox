1697113546a506e48b7d42f946ef9b500d5f879a
Vitaly Sizov <vitalii_sizov@mail.ru>
2017-08-21 22:49:54 +0600

    Chapter 19 completed

--- app/src/main/java/com/sizov/vitaly/beatbox/BeatBox.java
+++ app/src/main/java/com/sizov/vitaly/beatbox/BeatBox.java
@@ -1,7 +1,10 @@
 package com.sizov.vitaly.beatbox;
 
 import android.content.Context;
+import android.content.res.AssetFileDescriptor;
 import android.content.res.AssetManager;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.util.Log;
 
 import java.io.IOException;
@@ -12,14 +15,31 @@ public class BeatBox {
     private static final String TAG = "BeatBox";
 
     private static final String SOUND_FOLDER = "sample_sound";
+    private static final int MAX_SOUNDS = 5;
 
     private AssetManager mAssets;
     private List<Sound> mSounds = new ArrayList<>();
+    private SoundPool mSoundPool;
 
     public BeatBox(Context context) {
         mAssets = context.getAssets();
+        mSoundPool = new SoundPool(MAX_SOUNDS, AudioManager.STREAM_MUSIC, 0);
         loadSounds();
     }
+
+    public void play(Sound sound) {
+        Integer soundId = sound.getSoundId();
+        if (soundId == null) {
+            return;
+        }
+        mSoundPool.play(soundId, 1.0f, 1.0f, 1, 0, 1.0f);
+    }
+
+    // Освобождение SoundPool
+    public void release() {
+        mSoundPool.release();
+    }
+
 
     private void loadSounds() {
         String [] soundNames;
@@ -33,10 +53,22 @@ public class BeatBox {
 
         // Создание списка объектов Sound
         for (String filename : soundNames) {
-            String assetPath = SOUND_FOLDER + "/" + filename;
-            Sound sound = new Sound(assetPath);
-            mSounds.add(sound);
+            try {
+                String assetPath = SOUND_FOLDER + "/" + filename;
+                Sound sound = new Sound(assetPath);
+                load(sound);
+                mSounds.add(sound);
+            } catch (IOException ioe) {
+                Log.e(TAG, "Could not load sound " + filename, ioe);
+            }
         }
+    }
+
+    // Загрузка звуков в SoundPool
+    private void load(Sound sound) throws IOException {
+        AssetFileDescriptor afd = mAssets.openFd(sound.getAssetPath());
+        int soundId = mSoundPool.load(afd, 1);
+        sound.setSoundId(soundId);
     }
 
     public List<Sound> getSounds() {
